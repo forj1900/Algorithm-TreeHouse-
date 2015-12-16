56f778ef9c2465d26e4363928a620e9dc42a6cdf
forj1900 <ser-zhuchkov@yandex.ru>
2015-12-15 19:00:18 -0500

    Initial Commit

---
+++ Algorythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorythm
+//
+//  Created by Sergey1T on 15.12.15.
+//  Copyright © 2015 Sergey Zhuchkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorythm
+//
+//  Created by Sergey1T on 15.12.15.
+//  Copyright © 2015 Sergey Zhuchkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
